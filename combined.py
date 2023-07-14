c1840b32e7f7f2f1a97a69617f4675373ae276cc
Ahmedhassan676 <81989422+Ahmedhassan676@users.noreply.github.com>
2023-07-14 16:51:11 +0300

    Update combined.py

--- combined.py
+++ combined.py
@@ -2,12 +2,12 @@ from st_pages import Page,Section, show_pages, add_page_title
 show_pages(
     [
         Page("physical_prop.py", "Physical properties", "🏠"),
-        Page("comp.py", "Compressor Evaluation", "🏠"),
-        Page("heater.py", "Heater Efficiency", ":books:"),
+        Page("comp.py", "Compressor Evaluation", "🐔"),
+        Page("heater.py", "Heater Efficiency", "🐔"),
         
         # Pages after a section will be indented
-        Page("line.py", "Line Sizing tool",icon="💪"),
+        Page("line.py", "Line Sizing tool",icon="🐔"),
         # Unless you explicitly say in_section=False
-        Page("pump.py", "Cent. Pump performance test",icon="💪")
+        Page("pump.py", "Cent. Pump performance test",icon="🐔")
     ]
 )
